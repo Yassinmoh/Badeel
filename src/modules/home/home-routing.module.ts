58de255654c721807656b6c33bf509da90df3e61
Yassinmoh <yassin_mohamed310.com>
2024-01-23 22:37:59 +0200

    Feat submit page

--- src/modules/home/home-routing.module.ts
+++ src/modules/home/home-routing.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainLayoutComponent } from '../core/main-layout/main-layout.component';
+import { SubmitPageComponent } from './submit-page/submit-page.component';
 
 const routes: Routes = [
   {
     path:'',
-    component:MainLayoutComponent
-  }
+    component:MainLayoutComponent,
+    children:[
+      {
+        path:'submit',
+        component:SubmitPageComponent
+      }
+    ]
+  },
+
 ];
 
 @NgModule({
