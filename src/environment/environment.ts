8053df02cd015454fa0cec72366d5ce9fc224e37
Yassinmoh <yassin_mohamed310.com>
2024-01-29 14:49:18 +0200

    integrate app with firebase

---
+++ src/environment/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyBiOqZj5hMqYbwnPuy9jjak40oc3QnP4qI",
+    authDomain: "badeel-4ace6.firebaseapp.com",
+    projectId: "badeel-4ace6",
+    storageBucket: "badeel-4ace6.appspot.com",
+    messagingSenderId: "521573532508",
+    appId: "1:521573532508:web:4cd4696e469e9d8bee3799",
+    measurementId: "G-Y0LEF354NB"
+  }
+};
