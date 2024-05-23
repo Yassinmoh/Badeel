a0370f61616534cd487dcec355d0bebfb3b18341
ymohamed <ymohamed@sarmady.net>
2024-05-23 13:03:25 +0300

    Feat(Home): style no-data component

--- src/app/modules/shared/components/no-data-found/no-data-found.component.ts
+++ src/app/modules/shared/components/no-data-found/no-data-found.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { ProductState } from '../../../../store/products/product.reducer';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 import { getSerchTerm } from '../../../../store/products/product.selectors';
 import { CommonModule } from '@angular/common';
 
@@ -17,6 +17,12 @@ export class NoDataFoundComponent implements OnInit {
   store = inject(Store<ProductState>)
   searchTerm$!:Observable<string>
   ngOnInit(): void {
-    this.searchTerm$ = this.store.select(getSerchTerm)
+    this.searchTerm$ = this.store.select(getSerchTerm).pipe(
+      map(test => {
+        // Check search term text to prevent XSS attacks:
+        const regex = /[<>()"']/g
+        return test.replace(regex,'*')
+      })
+    )
   }
 }
