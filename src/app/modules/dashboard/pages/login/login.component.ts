4637b2ba2c39b5ae095747b79fa54e2aae9ea599
ymohamed <ymohamed@sarmady.net>
2024-05-27 17:15:44 +0300

    Style(Dashbord): Add style to login page

--- src/app/modules/dashboard/pages/login/login.component.ts
+++ src/app/modules/dashboard/pages/login/login.component.ts
@@ -1,12 +1,56 @@
-import { Component } from '@angular/core';
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { Component, HostListener, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
+  animations: [
+    trigger('slideInRight', [
+      state('start', style({ transform: 'translateX(-100%)' })),
+      state('end', style({ transform: 'translateX(0)' })),
+      transition('start => end', [
+        animate('1.5s')
+      ])
+    ]),
+    trigger('fadeIn', [
+      state('hidden', style({ opacity: 0 })),
+      state('visible', style({ opacity: 1 })),
+      transition('hidden => visible', [
+        animate('1.5s')
+      ])
+    ])
+  ]
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
 
+  rightState = 'start';
+  leftState = 'hidden';
+  isDesktop = false;
+  constructor(private renderer: Renderer2) {}
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event:Event) {
+    this.checkScreenSize();
+  }
+
+  ngOnInit() {
+    this.checkScreenSize();
+    if (this.isDesktop) {
+      setTimeout(() => {
+        this.rightState = 'end';
+        this.leftState = 'visible';
+      }, 1500);
+    }
+  }
+
+  checkScreenSize() {
+    this.isDesktop = window.innerWidth > 768;
+    if (!this.isDesktop) {
+      this.rightState = 'end';
+      this.leftState = 'visible';
+    }
+  }
 }
