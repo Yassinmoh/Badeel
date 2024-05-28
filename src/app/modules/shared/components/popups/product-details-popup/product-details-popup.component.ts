43e022102bd2d501bea96b8d9ef79bd60875795f
ymohamed <ymohamed@sarmady.net>
2024-05-28 11:52:15 +0300

    Feat(Products): Add products images and Fix details popup

--- src/app/modules/shared/components/popups/product-details-popup/product-details-popup.component.ts
+++ src/app/modules/shared/components/popups/product-details-popup/product-details-popup.component.ts
@@ -1,30 +1,34 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { fadeIn } from '../../../../core/animations';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-product-details-popup',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './product-details-popup.component.html',
   styleUrl: './product-details-popup.component.scss',
   animations:[fadeIn]
 })
+
 export class ProductDetailsPopupComponent {
   @Input() selectedProduct:any={}
   @Output() onClose = new EventEmitter<any>();
 
+
+
   close(){
     this.onClose.emit()
   }
-  getProductStatus(status:string):string{
+  getProductStatus(status:string){
     if(status == 'boycott'){
-      return 'داعم'
+      return { icon: 'B', name: 'داعم', textColor: '#e94444' }
     }else if(status == 'alternative'){
-      return 'بديل'
+      return { icon: 'A', name: 'بديل', textColor: '#9bc880' }
     }else if(status == 'unsure'){
-      return 'غير متأكد'
+      return { icon: 'C', name: 'قيد المراجعة', textColor: '#c6d315' }
     }else{
-      return ''
+      return {}
     }
   }
 
